389e87d9f079a7314e9e46b82e6f9d70dcefab68
Aina <vi.vi.1998@list.ru>
2023-05-21 17:18:00 +0300

    mainpage

--- client/src/components/Pages/MainPage.tsx
+++ client/src/components/Pages/MainPage.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import Img from '/monkeyWithRice.jpg';
+
+const textAnimation = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: (custom) => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.2 },
+  }),
+};
 
 export default function MainPage(): JSX.Element {
-  return <div>MainPage</div>;
+  return (
+    <motion.section initial="hidden" whileInView="visible">
+      <div>
+        <img src={Img} alt="hero" />
+      </div>
+      <article>
+        <motion.h1 custom={1} variants={textAnimation}>
+          More than just shorter links
+        </motion.h1>
+        <motion.p custom={2} variants={textAnimation}>
+          Build your brand's recognition and get detailed insights on how your links are performing.
+        </motion.p>
+        <motion.button custom={3} variants={textAnimation} size="large">
+          Get Started
+        </motion.button>
+      </article>
+    </motion.section>
+  );
 }
