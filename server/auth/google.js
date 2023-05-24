dfa7f0b38c96dce892bb97ecd587935b5826c5fc
Lena <lenamms@yandex.ru>
2023-05-24 17:42:39 +0300

    seeds users

--- server/auth/google.js
+++ server/auth/google.js
@@ -1,46 +1,46 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const { User } = require('../db/models');
+// const { User } = require('../db/models');
 
-const GOOGLE_CALLBACK_URL = 'http://localhost:3001/api/v1/auth/google/callback';
+// const GOOGLE_CALLBACK_URL = 'http://localhost:3001/api/v1/auth/google/callback';
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: GOOGLE_CALLBACK_URL,
-      passReqToCallback: true,
-      scope: ['profile', 'email'],
-    },
-    async (request, accessToken, refreshToken, profile, cb) => {
-      const defaultUser = {
-        email: profile.emails[0].value,
-        googleId: profile.id,
-      };
-      const user = await User.findOrCreate({
-        where: { googleId: profile.id },
-        defaults: defaultUser,
-      }).catch((err) => {
-        console.log('Signup error with Google!!!: ', err);
-        cb(err, null);
-      });
-      if (user && user[0]) return cb(null, user && user[0]);
-    },
-  ),
-);
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: GOOGLE_CALLBACK_URL,
+//       passReqToCallback: true,
+//       scope: ['profile', 'email'],
+//     },
+//     async (request, accessToken, refreshToken, profile, cb) => {
+//       const defaultUser = {
+//         email: profile.emails[0].value,
+//         googleId: profile.id,
+//       };
+//       const user = await User.findOrCreate({
+//         where: { googleId: profile.id },
+//         defaults: defaultUser,
+//       }).catch((err) => {
+//         console.log('Signup error with Google!!!: ', err);
+//         cb(err, null);
+//       });
+//       if (user && user[0]) return cb(null, user && user[0]);
+//     },
+//   ),
+// );
 
-passport.serializeUser((user, cb) => {
-  console.log('Serializing user: ', user);
-  cb(null, user.id);
-});
+// passport.serializeUser((user, cb) => {
+//   console.log('Serializing user: ', user);
+//   cb(null, user.id);
+// });
 
-passport.deserializeUser(async (id, cb) => {
-  const user = await User.findOne({ where: { id } }).catch((err) => {
-    console.log('Error deserializing: ', err);
-    cb(err, null);
-  });
-  console.log('DeSerialized user: ', user);
-  if (user) cb(null, user);
-});
+// passport.deserializeUser(async (id, cb) => {
+//   const user = await User.findOne({ where: { id } }).catch((err) => {
+//     console.log('Error deserializing: ', err);
+//     cb(err, null);
+//   });
+//   console.log('DeSerialized user: ', user);
+//   if (user) cb(null, user);
+// });
--- server/auth/passport.js
+++ server/auth/passport.js
@@ -1,19 +1,19 @@
-const passport = require("passport");
-const passportJwt = require("passport-jwt");
+// const passport = require("passport");
+// const passportJwt = require("passport-jwt");
 
-const { ExtractJwt } = passportJwt;
-const StrategyJwt = passportJwt.Strategy;
-const { User } = require("../db/models");
+// const { ExtractJwt } = passportJwt;
+// const StrategyJwt = passportJwt.Strategy;
+// const { User } = require("../db/models");
 
-passport.use(
-  new StrategyJwt(
-    {
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
-    },
-    (jwtPayload, done) =>
-      User.findOne({ where: { id: jwtPayload.id } })
-        .then((user) => done(null, user))
-        .catch((err) => done(err))
-  )
-);
+// passport.use(
+//   new StrategyJwt(
+//     {
+//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//       secretOrKey: process.env.JWT_SECRET,
+//     },
+//     (jwtPayload, done) =>
+//       User.findOne({ where: { id: jwtPayload.id } })
+//         .then((user) => done(null, user))
+//         .catch((err) => done(err))
+//   )
+// );
