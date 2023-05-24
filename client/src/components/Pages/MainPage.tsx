c28de19815c3f3f8ac5ff90ab67be7d159b0a768
Aina <vi.vi.1998@list.ru>
2023-05-24 09:36:12 +0300

    save

--- client/src/components/Pages/MainPage.tsx
+++ client/src/components/Pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import Img from '/work.jpg';
+// import Img from '/work.png';
 
 const textAnimation = {
   hidden: {
@@ -38,9 +38,7 @@ export default function MainPage(): JSX.Element {
           Начать
         </motion.button>
       </article>
-      <div>
-        <motion.img src={Img} alt="hero" custom={4} variants={textAnimation} />
-      </div>
+      <div>{/* <motion.img src={Img} alt="hero" custom={4} variants={textAnimation} /> */}</div>
     </motion.section>
   );
 }
